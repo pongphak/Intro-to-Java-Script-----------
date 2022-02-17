4e5616868f4ec505106a6d0d31930c12eaf6f48f
pongphak <94102896+pongphak@users.noreply.github.com>
2022-02-17 14:57:18 +0700

    initial commit

--- 28_Pongphak Phak.js
+++ 28_Pongphak Phak.js
@@ -1,12 +1,12 @@
 function askMyName() {
-    const name = // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
- 
+    const name = prompt('What Is Your Name');
+
     if (name != null) {
-       document.getElementById("ใส่ id ของ h1 ในนี้").innerHTML = "My name is " + name;
+       document.getElementById("PongphakPhak").innerHTML = "My name is " + name;
     }
  }
  
  function helloWorld() {
- const h1message = // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+ const h1message = document.getElementById("PongphakPhak").innerHTML
     alert(h1message + 'IG: pongphak97')
  }
